0970c5b4027cf66db4086f1c3b356383324454e3
Todd Baert <todd.baert@dynatrace.com>
2024-11-13 17:31:42 -0700

    fix: tracking (#14)
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -3,14 +3,18 @@
 import { NextResponse } from "next/server";
 import { OpenFeature } from "@openfeature/server-sdk";
 import { headerToEvaluationContext } from "@/libs/open-feature/evaluation-context";
+import { Product } from "@/types/product";
 
 export async function POST(request: Request) {
-  const featureFlagClient = OpenFeature.getClient(
-    headerToEvaluationContext(request.headers)
-  );
+  const context = headerToEvaluationContext(request.headers);
+  const featureFlagClient = OpenFeature.getClient(context);
   const order = await request.json();
   console.log("Order received:", order);
-  featureFlagClient.track("order_received");
+  featureFlagClient.track("order_received", context, {
+    value: order.items?.reduce((acc: number, i: Product) => {
+      return acc + i.price;
+    }, 0),
+  });
 
   return NextResponse.json({ message: "Order received successfully" });
 }
