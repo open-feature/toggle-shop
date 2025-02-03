5c8e3a8b84848c07de750eee1389e536b43b671c
Michael Beemer <beeme1mr@users.noreply.github.com>
2025-02-03 20:26:02 +0000

    feat: add codegen react
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,11 @@ import Banner from "@/components/Banner";
 import heroImage from "../../public/img/hero.jpg";
 
 import { useAutoAnimate } from "@formkit/auto-animate/react";
-import { useSuspenseFlag } from "@openfeature/react-sdk";
+import { useSuspenseOfferFreeShipping } from "@/generated/react-hooks";
 
 export default function Home() {
   const [parent] = useAutoAnimate();
-  const { value: showBanner } = useSuspenseFlag("offer-free-shipping", false);
+  const { value: showBanner } = useSuspenseOfferFreeShipping({});
 
   return (
     <>
